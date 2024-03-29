34e47463876c60dbf0bf9dda876a27b189f83424
Ivan Rubinov <linuxkernelpatch8234@riseup.net>
2024-03-29 19:41:37 +0300

    Фикс всех багов связанных с памятью task3

--- task3/main.c
+++ task3/main.c
@@ -8,17 +8,31 @@
     __typeof__(b) _b = (b);                                                    \
     _a < _b ? _a : _b;                                                         \
   })
+
+#define swap(x, y)                                                             \
+  do {                                                                         \
+    unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1];  \
+    memcpy(swap_temp, &y, sizeof(x));                                          \
+    memcpy(&y, &x, sizeof(x));                                                 \
+    memcpy(&x, swap_temp, sizeof(x));                                          \
+  } while (0)
+
 unsigned int *bubble_sort(char **a_lines, unsigned int lines,
                           unsigned int file_len);
+unsigned int *shaker_sort(char **a_lines, unsigned int lines,
+                          unsigned int file_len);
+
+unsigned int *insertion_sort(char **a_lines, unsigned int lines,
+                             unsigned int file_len);
 
 int main(int argc, char *argv[]) {
-  FILE *fptr;
-  size_t file_size;
-  char *buffer;
-  char **a_lines;
-  unsigned int lines;
-  unsigned int *len_lines;
-  unsigned int *addresses;
+  FILE *fptr = NULL;
+  unsigned int file_size = 0;
+  char *buffer = NULL;
+  char **a_lines = NULL;
+  unsigned int lines = 0;
+  unsigned int *len_lines = NULL;
+  unsigned int *addresses = NULL;
 
   if (argc != 4) {
     printf("Not valid count argumets\n");
@@ -36,7 +50,12 @@ int main(int argc, char *argv[]) {
   file_size = ftell(fptr);
   fseek(fptr, 0, SEEK_SET);
 
-  buffer = malloc(file_size);
+  if (file_size == 0) {
+    printf("File dont have content, exit\n");
+    exit(1);
+  }
+
+  buffer = calloc(file_size, 1);
 
   char ch;
   while (!feof(fptr)) {
@@ -46,19 +65,21 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  len_lines = malloc(lines * sizeof(int));
-  a_lines = malloc(lines * sizeof(char *));
+  len_lines = calloc(lines, sizeof(int));
+  a_lines = calloc(lines, sizeof(char *));
   fseek(fptr, 0, SEEK_SET);
 
   size_t curr_len = 0;
   unsigned int i = 0;
-  char *line;
+  char *line = NULL;
   while (!feof(fptr)) {
     ch = fgetc(fptr);
     buffer[curr_len] = ch;
     curr_len++;
     if (ch == '\n') {
-      line = malloc(curr_len * sizeof(char));
+      buffer[curr_len] = '\0';
+      curr_len++;
+      line = calloc(curr_len, sizeof(char));
       strncpy(line, (const char *)buffer, curr_len);
       a_lines[i] = line;
       len_lines[i] = curr_len;
@@ -66,23 +87,56 @@ int main(int argc, char *argv[]) {
       curr_len = 0;
     }
   }
+  free(buffer);
 
-  addresses = bubble_sort(a_lines, lines, file_size);
+  fclose(fptr);
+  switch (*argv[3]) {
+  case '1':
+    addresses = bubble_sort(a_lines, lines, file_size);
+    break;
+  case '2':
+    addresses = shaker_sort(a_lines, lines, file_size);
+    break;
+  case '3':
+    addresses = insertion_sort(a_lines, lines, file_size);
+    break;
+
+  default:
+    printf("Not valid type of sort\n");
+    exit(1);
+  }
+
+  fptr = fopen(argv[2], "w");
+
+  if (fptr == NULL) {
+    printf("Problems to write with file with name %s, exit\n", argv[2]);
+    exit(1);
+  }
 
   for (int i = 0; i < lines; i++) {
-    printf("%s", a_lines[addresses[i]]);
+    if (a_lines[addresses[i]][0] != '\n' && a_lines[addresses[i]][0] != '\0') {
+      fputs(a_lines[addresses[i]], fptr);
+      // fprintf(fptr, "%s", a_lines[addresses[i]]);
+      printf("%s", a_lines[addresses[i]]);
+    }
+    free(a_lines[addresses[i]]);
   }
+
+  fclose(fptr);
+
+  free(addresses);
+  free(len_lines);
+  free(a_lines);
 
   return EXIT_SUCCESS;
 }
 
 unsigned int *bubble_sort(char **a_lines, unsigned int lines,
                           unsigned int file_len) {
-  unsigned int *addresses;
-  bool f;
-  int temp;
+  unsigned int *addresses = calloc(lines, sizeof(unsigned int));
+  bool f = 0;
+  int temp = 0;
 
-  addresses = malloc(lines * sizeof(int));
   for (unsigned int i = 0; i < lines; i++) {
     addresses[i] = i;
   }
@@ -90,18 +144,68 @@ unsigned int *bubble_sort(char **a_lines, unsigned int lines,
   for (unsigned int j = 1; j < lines; j++) {
     f = 0;
     for (unsigned int i = 0; i < lines - j; i++) {
-
-      if (strncmp(a_lines[addresses[i]], a_lines[addresses[i + 1]],
-                  min(strlen(a_lines[addresses[i]]),
-                      strlen(a_lines[addresses[i + 1]]))) > 0) {
-        temp = addresses[i];
-        addresses[i] = addresses[i + 1];
-        addresses[i + 1] = temp;
+      if (a_lines[addresses[i]][0] == '\n') {
+        swap(addresses[i], addresses[i + 1]);
+        f = 1;
+      } else if (a_lines[addresses[i + 1]][0] == '\n') {
+      } else if (strcmp(a_lines[addresses[i]], a_lines[addresses[i + 1]]) > 0) {
+        swap(addresses[i], addresses[i + 1]);
         f = 1;
       };
     }
     if (f == 0) {
       break;
+    }
+  }
+  return addresses;
+}
+unsigned int *shaker_sort(char **a_lines, unsigned int lines,
+                          unsigned int file_len) {
+  unsigned int *addresses = NULL;
+  bool f;
+  int temp;
+
+  addresses = calloc(lines, sizeof(int));
+  for (unsigned int i = 0; i < lines; i++) {
+    addresses[i] = i;
+  }
+
+  int left = 0;
+  int right = lines - 1;
+  while (left <= right) {
+    for (int i = right; i > left; --i) {
+      if (strcmp(a_lines[addresses[i - 1]], a_lines[addresses[i]]) > 0) {
+        swap(addresses[i - 1], addresses[i]);
+      }
+    }
+    ++left;
+    for (int i = left; i < right; ++i) {
+      if (strcmp(a_lines[addresses[i]], a_lines[addresses[i + 1]]) > 0) {
+        swap(addresses[i], addresses[i + 1]);
+      }
+    }
+    --right;
+  }
+  return addresses;
+}
+
+unsigned int *insertion_sort(char **a_lines, unsigned int lines,
+                             unsigned int file_len) {
+  unsigned int *addresses = NULL;
+  bool f;
+  int temp;
+
+  addresses = calloc(lines, sizeof(int));
+  for (unsigned int i = 0; i < lines; i++) {
+    addresses[i] = i;
+  }
+
+  for (int i = 1; i < lines; i++) {
+    int j = i - 1;
+    while (j >= 0 &&
+           strcmp(a_lines[addresses[j]], a_lines[addresses[j + 1]]) > 0) {
+      swap(addresses[j], addresses[j + 1]);
+      j--;
     }
   }
   return addresses;
